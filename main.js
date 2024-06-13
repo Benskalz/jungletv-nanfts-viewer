f7e7d88280e04501687555d120499a1e6007b0e5
Benjamin <22433963+Benskalz@users.noreply.github.com>
2024-06-14 00:53:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+const pages = require("jungletv:pages");
+const configuration = require("jungletv:configuration")
+
+pages.publishFile("nanfts", "nanfts.html", "NaNFT");
+configuration.setProfileTab("nanfts")
